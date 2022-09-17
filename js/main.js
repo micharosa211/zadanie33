6ebb8cf47d8b751dd878c5f1d09c47cb79bbeac4
michalrosa211 <michal.rosadzinski@gmail.com>
2022-09-17 19:06:01 +0200

    klik wyświetla dodatkowy opis

---
+++ js/main.js
@@ -0,0 +1,8 @@
+element = document.getElementById('opis');
+console.log(element);
+button = document.querySelector('button');
+button.onclick = function() { 
+document.write("przykładowy tekst");
+}
+
+
